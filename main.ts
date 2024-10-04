c279b660bb6aa1ea8f01458d3101c9435674f684
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-10-04 16:46:39 +0200

    Placeholder functionality added

--- main.ts
+++ main.ts
@@ -2,8 +2,29 @@ import { Plugin } from 'obsidian';
 
 export default class ChroniclePlugin extends Plugin {
 
-    onload(): Promise<void> | void {
-        console.log('test')
+    statusBarTextElement: HTMLSpanElement;
+
+    onload() {
+        this.statusBarTextElement = this.addStatusBarItem().createEl('span');
+
+        this.app.workspace.on('active-leaf-change', async () => {
+            const file = this.app.workspace.getActiveFile();
+            if(file) {
+                const content = await this.app.vault.read(file);
+                this.statusBarTextElement.textContent = `You've got ${this.getLineCount(content)} lines`;
+            }
+        })
+
+        this.app.workspace.on('editor-change', editor => {
+            const document = editor.getDoc().getValue();
+            this.statusBarTextElement.textContent = `You've got ${this.getLineCount(document)} lines`;
+        });
+    }
+
+    private getLineCount(fileContent?: string) {
+        return fileContent
+            ? fileContent.split(/\r\n|\r|\n/).length
+            : 0;
     }
 
 };
