349512d82a74e48198f699e3ef9bdd5c04935bf1
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-10-04 22:19:00 +0200

    Revert "Calendar view"
    This reverts commit 4f5c136fbe137c2423ea7fbcf5b858da58394053.

--- src/chronicle_view.ts
+++ src/chronicle_view.ts
@@ -1,6 +1,3 @@
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
 import { IconName, ItemView, WorkspaceLeaf } from 'obsidian';
 
 export const VIEW_TYPE = 'chronicle-view';
@@ -27,44 +24,9 @@ export default class ChronicleView extends ItemView {
     async onOpen() {
         const container = this.containerEl.children[1];
         container.empty();
-
         const content = document.createElement('div');
-        const calendar = new Calendar(content, {
-            plugins: [ dayGridPlugin, timeGridPlugin ],
-            initialView: 'timeGridWeek',  // Default view is Month view
-            headerToolbar: {
-                left: 'prev,next today',  // Navigation buttons
-                center: 'title',          // Calendar title (month/year)
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'  // View options: Month, Week, Day
-            },
-            selectable: true,  // Allow selecting time slots
-            editable: true,    // Allow events to be dragged and resized
-            events: [
-                // Array of event objects to display on the calendar
-                {
-                    title: 'Event 1',
-                    start: '2024-10-04',
-                    end: '2024-10-05'
-                },
-                {
-                    title: 'Event 2',
-                    start: '2024-10-10',
-                    end: '2024-10-12'
-                }
-            ],
-            // dateClick: function(info) {
-            //     // Triggered when a date is clicked
-            //     alert('Date clicked: ' + info.dateStr);
-            // },
-            eventClick: function(info) {
-                // Triggered when an event is clicked
-                alert('Event: ' + info.event.title);
-            }
-        });
-
+        content.textContent = 'Content biatch';
         container.appendChild(content);
-        calendar.render();
-        calendar.updateSize();
     }
 
 }
