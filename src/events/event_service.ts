b91f0209422c04d8a6e956d7592f2bef554a29ce
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-10-28 07:01:33 +0100

    EventService to functional

--- src/events/event_service.ts
+++
@@ -1,7 +0,0 @@
-import { CalendarApi, EventInput } from "@fullcalendar/core";
-
-export default class EventService {
-    createEvent(calendar: CalendarApi, eventInput: EventInput) {
-        return calendar.addEvent(eventInput);
-    }
-}
