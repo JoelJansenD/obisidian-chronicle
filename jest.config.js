2a6a1d3ca24d9ea40b44888043c1ec58f53a16b2
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-11-01 21:30:14 +0100

    Calendar tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',                // Use ts-jest to handle TypeScript files
   testEnvironment: 'node',           // Use Node.js environment for testing (you can switch to 'jsdom' if needed)
-  roots: ['src/', 'tests/'],  // Directories to look for source and test files
+  roots: ['src/'],  // Directories to look for source and test files
   testMatch: ['**/*.test.ts'],    // Pattern to find test files (adjust as needed)
   moduleDirectories: ['node_modules', 'src'],     // Allow Jest to resolve modules from /src/
   moduleNameMapper: {                 // Map paths (if you're using TypeScript path aliases)
