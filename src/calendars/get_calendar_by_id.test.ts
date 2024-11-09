7b65e7755190fcb0944a2d400876f9573b773622
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-11-09 15:07:09 +0100

    Refactor

--- src/calendars/get_calendar_by_id.test.ts
+++ src/calendars/get_calendar_by_id.test.ts
@@ -1,45 +1,5 @@
-import { ChronicleSettings } from '@src/settings/obsidian_chronicle_settings';
-import { getCalendarById, getCalendars } from './calendars';
+import { getCalendarById } from './get_calendar_by_id';
 import { Guid } from 'guid-typescript';
-
-describe('getCalendars', () => {
-    it('should return all calendars', () => {
-        // Arrange
-        const config : ChronicleSettings = {
-            calendars: [
-                {
-                    id: Guid.raw(),
-                    colour: '#ffffff',
-                    directory: '',
-                    name: 'cal1'
-                },
-                {
-                    id: Guid.raw(),
-                    colour: '#ffffff',
-                    directory: '',
-                    name: 'cal2'
-                },
-            ]
-        };
-
-        // Act
-        const result = getCalendars(config);
-
-        // Assert
-        expect(result).toEqual(config.calendars);
-    });
-    
-    it('should return empty array when no calendars are known', () => {
-        // Arrange
-        const config : ChronicleSettings = { calendars: [] };
-
-        // Act
-        const result = getCalendars(config);
-
-        // Assert
-        expect(result).toEqual([]);
-    });
-});
 
 describe('getCalendarById', () => {
     it('should return the calendar with the given id', () => {
