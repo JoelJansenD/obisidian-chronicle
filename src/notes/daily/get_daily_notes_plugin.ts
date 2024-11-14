3d255bfedd57b807015a84d15b9a75af4a97fce1
Joël Jansen Duighuizen <joel.duighuizen@koop.overheid.nl>
2024-11-14 18:15:13 +0100

    getDailyNotesPlugin

--- src/notes/daily/get_daily_notes_plugin.ts
+++ src/notes/daily/get_daily_notes_plugin.ts
@@ -1,7 +1,7 @@
 import { App } from "obsidian";
 import DailyNotesPlugin from "./daily_notes_plugin";
 
-export default function getDailyNotesPlugin(app: App) : DailyNotesPlugin {
+export default function getDailyNotesPlugin(app: App) : DailyNotesPlugin | null {
     const plugins = (app as any).internalPlugins;
-    return plugins.getPluginById('daily-notes');
+    return plugins.getPluginById('daily-notes') ?? null;
 }
